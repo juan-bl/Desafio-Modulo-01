e4338a57e72840fb906a61ab2ec3129feb982598
juan-bl <juannicolasdev@gmail.com>
2023-05-10 18:55:44 -0300

    Desafio concluido

--- 12 - cAPS LOCK oN/index.js
+++ 12 - cAPS LOCK oN/index.js
@@ -1,60 +1,38 @@
-const input = "cApS"
+function processData(input) {
+    //Enter your code here
+    let contador = 0, inputCorrigido = '';
 
-// 1 forma
+    // Precisa ter no maximo 5000 caracteres
+    if (input.length <= 5000) {
 
-let resposta;
-if (input[0] === input[0].toLowerCase() && input.slice(1) === input.slice(1).toUpperCase()) {
-    resposta = input.slice(0, 1).toUpperCase() + input.slice(1).toLowerCase()
-}   else if (input === input.toUpperCase()) {
-    resposta = input.toLowerCase()
-} else {
-    resposta = input
-}
-console.log(resposta)
+        // CAPS precisa ser caps
+        if (input === input.toUpperCase()) {
+            console.log(input.toLowerCase());
 
-// 2 forma
+        } else if (input[0] === input[0].toLowerCase() && input[1] === input[1].toUpperCase()) {
+            
+            for (let letra of input.trim()) {
+                if (letra === letra.toLowerCase()) {
+                    contador++;
+                };
+            };
 
-const a = input.trim().split('')
-const quantidadeDeLetras = a.length - 1
+            // cAPS precisa ser Caps
+            if (contador === 1) {
+                inputCorrigido = input.toLowerCase();
+                inputCorrigido = inputCorrigido.replace(inputCorrigido[0], inputCorrigido[0].toUpperCase());
+                console.log(inputCorrigido);
 
-const minusculas = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
-const maiusculas = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-let contadorDeTrue = 0
-let contadorDeFalse = 0 
-
-const contar = a.map((letra) => {maiusculas.includes(letra) ? contadorDeTrue++ : contadorDeFalse++}) 
-
-if (contadorDeFalse === 0 || (minusculas.includes(input[0]) && quantidadeDeLetras === contadorDeTrue)) {
-    const resultado = a.map((letra) => {
-        if (maiusculas.includes(letra)) {
-    
-            return letra.toLowerCase()
-    
-        } else if (minusculas.includes(letra)) {
-    
-            return letra.toUpperCase()
-    
+            } else {
+               // cApS precisa ser cApS
+               console.log(input);
+            };
+            
         } else {
-            return letra
-        }
-    })  
-console.log(resultado.join(''))
-} else {
-    console.log(input)
-}
+            // caps continua caps
+            console.log(input);
+        };
+    };
+};
 
-// 3 forma 
-
-let resultado; 
-
-if (input[0] === input[0].toLowerCase() && input.slice(1) === input.slice(1).toUpperCase()) {
-    resultado = input.replace(/([a-z]{1})([A-Z]{1,100000})/, function(regex, argumento1, argumento2) {
-    return argumento1.toUpperCase() + argumento2.toLowerCase()
-}) 
-} else if (input === input.toUpperCase()) {
-    resultado = input.toLowerCase()
-} else {
-    resultado = input
-}
-console.log(resultado)
+processData('cAPS');
