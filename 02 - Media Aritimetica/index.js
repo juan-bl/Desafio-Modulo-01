719f3a928e502b73e15ac966fe8195dfbc263fb6
juan-bl <juannicolasdev@gmail.com>
2023-05-10 16:34:58 -0300

    Desafio concluido

--- 02 - Media Aritimetica/index.js
+++ 02 - Media Aritimetica/index.js
@@ -1,6 +1,15 @@
-const input = [2, 3, 4]
+function solucao(lista) {
+    //seu codigo aqui
+    let contator = 1;
 
-const resultado = input.reduce((acc, cur) => {
-    return acc + cur
-})
-console.log(resultado / input.length)
+    const resultado = lista.reduce((acumulador, elementoAtual) => {
+        contator++;
+        return acumulador + elementoAtual;
+    })
+
+    console.log(resultado/contator);
+}
+
+const lista = [2, 3, 4];
+
+solucao(lista);
