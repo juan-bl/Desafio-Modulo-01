1b26d393b08a7f177b7607c39e64ab14d451708d
juan-bl <juannicolasdev@gmail.com>
2023-05-10 16:46:15 -0300

    Desafio concluido

--- 04 - Mesa de Poker/index.js
+++ 04 - Mesa de Poker/index.js
@@ -1,10 +1,10 @@
-const inputValores = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const inputMin = 2
-const inputMax = 6
-
-const resultado = inputValores.filter((numeros) => {
-    return numeros >= inputMin && numeros <= inputMax
-    
+function solucao(min, max, valores) {
+    //seu codigo aqui
+    const resulatdo = valores.filter((numero) => {
+        return numero >= min && numero <= max;
     })
-    
-    console.log(resultado)
+
+    console.log(resulatdo);
+}
+
+solucao(2, 10, [0, 5, 6, 10, 11]);
