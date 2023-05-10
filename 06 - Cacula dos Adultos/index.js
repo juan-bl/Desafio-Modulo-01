bbcd27995df86c67cb3c0c40635f384718da0a21
juan-bl <juannicolasdev@gmail.com>
2023-05-10 17:01:09 -0300

    Desafio concluido

--- 06 - Cacula dos Adultos/index.js
+++ 06 - Cacula dos Adultos/index.js
@@ -1,28 +1,19 @@
-const lista = [1, 2, 1, 6, 9, 8, 12]
+function solucao(lista) {
+    // seu codigo aqui
+    let menor = Math.max(...lista), resultado = [];
+    
+    for (let numero of lista) {
+        if (menor >= numero && numero >= 18) {
+            menor = numero;
+            resultado.push(menor);
+        };
+    };
 
-const maiores = inputLista.filter((numeros) => {
-    return numeros >= 18
-})
-const resultado = maiores.sort((a, b) => {
-    return a - b
-})
+    if (resultado.length === 0) {
+        console.log('CRESCA E APARECA');
+    } else {
+        console.log(Math.min(...resultado));
+    };
+};
 
-resultado[0] === undefined ? console.log("CRESCA E APARECA") : console.log(resultado[0]);
-
-// 2 forma
-
-const maiores2 = inputLista.filter((numeros) => {
-    return numeros >= 18
-})
-
-const novaArray = []
-for (let i = 0; i < maiores2.length; i++) {
-    let ordem = maiores2[i]
-    for(let j = i+1; j < maiores2.length; j++) {
-        if (maiores2[j] < ordem) {
-            ordem = maiores2
-        }
-    }
-    novaArray.push(ordem);
-}
-novaArray[0] === undefined ? console.log("CRESCA E APARECA") : console.log(novaArray[0]);
+solucao([12, 18, 27]);
